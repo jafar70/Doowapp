4815dea96258e8cf2aaff7b36796266f45644e92
Jafar Salami <jafsalami@hotmail.co.uk>
2016-06-22 21:57:13 +0000

    final index page

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// JavaScript File
+$("#tohome").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#wrapper1").offset().top
+    }, 2000);
+});
+
+$("#tocurate").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#wrapper2").offset().top
+    }, 2000);
+});
+
+$("#tobarz").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#wrapper3").offset().top
+    }, 2000);
+});
+
+$("#toown").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#wrapper4").offset().top
+    }, 2000);
+});
+
+$("#tocontact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#wrapper5").offset().top
+    }, 2000);
+});
